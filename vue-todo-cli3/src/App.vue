5ce8dc040c09facec2ea41e7db5dacf810b4a8d2
ABarthDew <vsevolod117@gmail.com>
2021-12-08 22:31:12 +0900

    commit

--- vue-todo-cli3/src/App.vue
+++ vue-todo-cli3/src/App.vue
@@ -23,3 +23,15 @@ export default {
   }
 }
 </script>
+
+<style>
+input {
+  border-style: groove;
+}
+button {
+  border-style: groove;
+}
+.shadow {
+  box-shadow: 5px 10px 10px rgba(0,0,0,0.03);
+}
+</style>
--- vue-todo-cli3/src/components/TodoHeader.vue
+++ vue-todo-cli3/src/components/TodoHeader.vue
@@ -1,5 +1,13 @@
 <template>
-  <div>
-    TodoHeader
-  </div>
+  <header>
+    <h1>TODO it!</h1>
+  </header>
 </template>
+
+<style scoped>
+  h1 { 
+    color: mediumseagreen;
+    font-weight: bold;
+    margin: 10px;
+    }
+</style>
