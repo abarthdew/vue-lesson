1ba9b33a07391fbe4ddfac4855725e7ea364d0ba
ABarthDew <vsevolod117@gmail.com>
2021-12-11 23:38:27 +0900

    07_todo_list

--- vue-todo-cli3/src/components/TodoLIst.vue
+++ vue-todo-cli3/src/components/TodoLIst.vue
@@ -1,5 +1,27 @@
 <template>
   <div>
-    TodoList
+    <ul>
+      <li v-for="item in todoItem" :key="item">{{item}}</li>
+    </ul>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      todoItem: [],
+    }
+  },
+  created() {
+    if (localStorage.length > 0) {
+      for (var i = 0; i < localStorage.length; i++) {
+        if (localStorage.key(i) !== 'loglevel:webpack-dev-server') {
+        this.todoItem.push(localStorage.key(i));
+        console.log(localStorage.key(i));
+        }
+      }
+    }
+  }
+}
+</script>
