af0ab13bb5f2696bbd056a171ad6b3486eee452e
ABarthDew <vsevolod117@gmail.com>
2021-12-22 22:15:28 +0900

    21_vuex_state

--- vue-todo-cli3/src/App.vue
+++ vue-todo-cli3/src/App.vue
@@ -15,7 +15,7 @@
 import TodoFooter from './components/TodoFooter.vue'
 import TodoHeader from './components/TodoHeader.vue'
 import TodoInput from './components/TodoInput.vue'
-import TodoList from './components/TodoLIst.vue'
+import TodoList from './components/TodoList.vue'
 
 export default {
   name: 'App',
