63de32102947cdd3c17c6a635eceebc105c772e5
ABarthDew <vsevolod117@gmail.com>
2021-12-25 23:57:20 +0900

    25_vuex_helper_modules_3

--- vue-todo-cli3/src/store/store.js
+++ vue-todo-cli3/src/store/store.js
@@ -1,29 +1,15 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import { state } from './state'
 import * as getters from './getters'
-import * as mutations from './mutations';
+import * as mutations from './mutations'
 
 Vue.use(Vuex); // use vue plugin
 
-const storage = {
-    fetch() {
-        const arr = [];
-        if (localStorage.length > 0) {
-            for (let i = 0; i < localStorage.length; i++) {
-              if (localStorage.key(i) !== 'loglevel:webpack-dev-server') {
-              arr.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
-              }
-            }
-          }
-          return arr;
-    },
-}
+
 
 export const store = new Vuex.Store({ // expoer store : any component can use this object with function out of this file
-    state: {
-        headerText: 'TODO it!',
-        todoItems: storage.fetch(),
-    },
+    state: state,
     getters: getters,
     mutations: mutations,
 });
